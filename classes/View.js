60c591a4844e8cef553cc7232d3d83b67d03c6f2
Theo Knoepflin <theo.knoepflin@franceselection.fr>
2022-11-04 12:52:19 +0100

    Ajout d'un lien 'details' dans le menu more-options

--- classes/View.js
+++ classes/View.js
@@ -50,6 +50,7 @@ class View {
                                             <!-- <button class="delete-btn" id="${books_list[i].isbn}"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></button> -->
                                             <div class="visible"> ⁝ </div>
                                             <div class="openable">
+                                                <button class="details-btn">Details</button>
                                                 <button class="share-btn">Partager</button>
                                                 <button class="delete-btn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 6h18"></path><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>&nbsp;Supprimer</button>
                                             </div>
@@ -150,6 +151,16 @@ class View {
                     new UserChoice(err, null, "OK").waitFor();  
                 }
                 // new UserChoice('🚧 Cette action n\'est pas encore disponible', null, "OK").waitFor();  
+            })
+        }
+
+
+        // Handle DETAILS 
+        const allDetailsBtn = document.querySelectorAll('#reading-list .book-entry button.details-btn');
+        for (let i = 0 ; i < allDetailsBtn.length; i++ ) {
+            allDetailsBtn[i].addEventListener('click', event => {
+                event.stopPropagation(); 
+                new UserChoice('🚧 Cette action n\'est pas encore disponible', null, "OK").waitFor();  
             })
         }
 
