cf325f6897481dfb523c2ad60ff8edad40b6ac85
Theo Knoepflin <theo.knoepflin@gmail.com>
2024-02-26 21:23:07 +0100

    Petites améliorations

--- classes/View.js
+++ classes/View.js
@@ -627,7 +627,8 @@ VIEWS
             </form>
             <div id="search-results"></div>
             <hr/>
-            <a id="add-form-cta" href="#/add-form">Ajout manuel</a>
+            <a id="add-form-cta" href="#/add-form"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>&nbsp;Ajout manuel</a>
+            <a id="add-barcode" href="#/add-barcode"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-qr-code"><rect width="5" height="5" x="3" y="3" rx="1"/><rect width="5" height="5" x="16" y="3" rx="1"/><rect width="5" height="5" x="3" y="16" rx="1"/><path d="M21 16h-3a2 2 0 0 0-2 2v3"/><path d="M21 21v.01"/><path d="M12 7v3a2 2 0 0 1-2 2H7"/><path d="M3 12h.01"/><path d="M12 3h.01"/><path d="M12 16v.01"/><path d="M16 12h1"/><path d="M21 12v.01"/><path d="M12 21v-1"/></svg>&nbsp;Scan de code-barre</a>
         </div>`; 
 
         CONTAINER.innerHTML = HTMLContent; 
@@ -660,6 +661,11 @@ VIEWS
         })
         document.querySelector('#query').addEventListener('blur', () => {
             document.querySelector('#search-form').classList.remove('focus'); 
+        })
+
+        document.querySelector('#add-barcode').addEventListener('click', event => {
+            event.preventDefault(); 
+            new UserChoice('⏳ Bientôt disponible', 'Compris').waitFor(); 
         })
 
 
@@ -1395,8 +1401,8 @@ TEMPLATES
                                             <h3 class="title">${books_list[i].title}</h3>
                                             <h4 class="author">${books_list[i].author}</h4>
                                             <p class="description maxheight0">${books_list[i].description}</p>
-                                            <p class="publisher"><strong>${books_list[i].publisher}</strong></p>
-                                            ${books_list[i].owned ? owned_icon + ' ' : ''}
+                                            <!-- <p class="publisher"><strong>${books_list[i].publisher}</strong></p> -->
+                                            <!-- ${books_list[i].owned ? owned_icon + ' ' : ''} -->
                                             ${ dateAdded }                                            
                                         </div>
                                         <div class="more-actions-button" id="id${books_list[i].google_id}">
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-/** Last update: 2024.02.25.001
+/** Last update: 2024.02.26.001
  */ 
 
 self.addEventListener("install", function (event) {
